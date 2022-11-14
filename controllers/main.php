af0516065ed12962782677387b008d9afc9944a3
Carlcuadros2103 <55800684+Carlcuadros2103@users.noreply.github.com>
2022-11-13 21:46:37 -0500

    Main
    Eliminacion de codigo comentado y saltos de linea

--- controllers/main.php
+++ controllers/main.php
@@ -2,16 +2,10 @@
 class Main extends Controller{
     function __construct(){
         parent::__construct();
-        //$this->view->validateSesion=$session->validateSesion();
-        //$this->view->mensaje = "Hay un error al cargar el recurso";
-        
-        //echo "<p>Controlador Index</p>";
     }
-
     function render(){
         $this->view->mensaje = "Hay un error al cargar el recurso";
         $this->view->render('main/index');
     }
 }
-
 ?>
