3bb27a1bc553a9f9fbeb9e2aedd3be5cc2af338e
Iomar Igor Alegre Barrera <42124277+iomarigor@users.noreply.github.com>
2022-11-13 22:38:16 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,24 +1,19 @@
 <?php
-
 define('URL', 'http://localhost/rifa-ccff/');
-
 define('HOST', 'localhost');
 /** Database username */
 // MYSQl = 3306
 // PostgreSQL = 5432
 // SQL Server = 1433
 define('DB_PORT', '3306');
-
 //////////////Select BD////////////
 //** 0 = MySQL || Maria DB
 //** 1 = PostgreSQL
 //** 2 = msSQl || SQL server
 define('DB_SELECT',0);
-
 define('PORT', '3306');
 define('DB_NAME', 'rifa_ccff');
 define('USER', 'root');
 define('PASSWORD', "");
 define('CHARSET', 'utf8mb4');
-
 ?>
